c3a9655642fa99568618d45d153a48301b18a6e0
Alexander Kombeiz <alexkombie@gmail.com>
2024-04-25 09:07:44 +0200

    added scripts for dwh migration

---
+++ dwh-migration/apply_aktin_backup.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+#--------------------------------------
+# Script Name:  apply_aktin_backup.sh
+# Author:       akombeiz@ukaachen.de
+# Date:         25 Apr 24
+# Purpose:      Installs a new AKTIN DWH from repository
+#               and fills it with backed up data from a
+#               given tar.gz file
+#--------------------------------------
+
+set -euo pipefail
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+# ensure the correct number of arguments is provided
+[[ $# -ne 1 ]] && {
+    echo "Usage: $0 <path_to_backup_tarfile>"
+    exit 1
+}
+
+readonly tarfile="$1"
+
+# create log file
+readonly log=apply_aktin_backup_$(date +%Y_%h_%d_%H%M).log
+
+# get wildfly home directory
+if [[ -d /opt/wildfly/ ]]; then
+    readonly wildfly_home=/opt/wildfly
+elif [[ -n $(ls /opt/wildfly-*) ]]; then
+    readonly wildfly_home=/opt/wildfly-*
+fi
+
+extract_tarfile_and_get_folder_path() {
+    tarfile="$1"
+    # create a temporary directory for extraction
+    temp_dir=$(mktemp -d)
+    # extract the archive into the temporary directory
+    tar -xf "$tarfile" -C "$temp_dir"
+    # check if exactly one folder exists in the extracted content
+    folder=$(find "$temp_dir" -mindepth 1 -maxdepth 1 -type d)
+    if [[ $(find "$temp_dir" -mindepth 1 -maxdepth 1 -type d | wc -l) -ne 1 ]]; then
+        echo "Error: Archive contains multiple folders or no folders"
+        rm -rf "$temp_dir"  # Clean up temp directory
+        return 1
+    fi
+    # return the path to the extracted folder
+    echo "$folder"
+}
+
+remove_dir() {
+    rm -rf $1
+}
+
+install_aktin_packages() {
+    echo "registering aktin repository"
+    apt-get update && apt-get install -y ca-certificates
+    # echo  "Acquire { https::Verify-Peer false }" > /etc/apt/apt.conf.d/99verify-peer.conf
+    wget -O - https://www.aktin.org/software/repo/org/apt/conf/aktin.gpg.key | sudo apt-key add -
+    echo "deb https://www.aktin.org/software/repo/org/apt focal main" >/etc/apt/sources.list.d/aktin.list
+    echo "installing aktin packages"
+    apt-get update
+    apt-get install -y aktin-notaufnahme-i2b2
+    apt-get install -y aktin-notaufnahme-dwh
+    apt-get install -y aktin-notaufnahme-updateagent
+}
+
+stop_aktin_services() {
+    echo "stoping aktin services"
+    service apache2 stop
+    service wildfly stop
+    service postgresql start
+}
+
+patch_aktin_properties() {
+    local backup_folder=$1
+    echo "patching aktin.properties with backup"
+    while read -r line1; do
+        if [[ ! $line1 = \#* && ! -z $line1 ]]; then
+            key=${line1%=*}
+            value=${line1#*=}
+            while read -r line2; do
+                if [[ ! $line2 = \#* && ! -z $line2 ]]; then
+                    if [[ ${line2%=*} == $key ]]; then
+                        sed -i "s|${key}=.*|${key}=${value}|" /etc/aktin/aktin.properties
+                        break
+                    fi
+                fi
+            done </etc/aktin/aktin.properties
+        fi
+    done <$backup_folder/backup_aktin.properties
+    chown wildfly:wildfly /etc/aktin/aktin.properties
+}
+
+patch_wilfly_conf() {
+    local backup_folder=$1
+    echo "patching wilfly configuration with backup"
+    cp $backup_folder/backup_standalone.xml $wildfly_home/standalone/configuration/standalone.xml
+    chown wildfly:wildfly $wildfly_home/standalone/configuration/standalone.xml
+    cp $backup_folder/backup_standalone.conf $wildfly_home/bin/standalone.conf
+    chown wildfly:wildfly $wildfly_home/bin/standalone.conf
+}
+
+import_folder_backup() {
+    local backup_folder=$1
+    local source_folder=$2
+    echo "importing backup of $source_folder"
+    cp -r $backup_folder$source_folder $source_folder
+    chown -R wildfly:wildfly $source_folder
+}
+
+import_databases_backup() {
+    local backup_folder=$1
+    echo "deleting aktin and i2b2 databases"
+    sudo -u postgres psql -c "DROP DATABASE IF EXISTS aktin; DROP USER IF EXISTS aktin;"
+    sudo -u postgres psql -c "DROP DATABASE IF EXISTS i2b2; DROP USER IF EXISTS i2b2crcdata; DROP USER IF EXISTS i2b2hive; DROP USER IF EXISTS i2b2imdata; DROP USER IF EXISTS i2b2metadata; DROP USER IF EXISTS i2b2pm; DROP USER IF EXISTS i2b2workdata;"
+    echo "reinitialising aktin and i2b2 databases"
+    sudo -u postgres psql -c "CREATE DATABASE aktin; \connect aktin; CREATE USER aktin with PASSWORD 'aktin'; CREATE SCHEMA AUTHORIZATION aktin; GRANT ALL ON SCHEMA aktin to aktin; ALTER ROLE aktin WITH LOGIN;"
+    sudo -u postgres psql -c "CREATE DATABASE i2b2; \connect i2b2; CREATE USER i2b2crcdata WITH PASSWORD 'demouser'; CREATE USER i2b2hive WITH PASSWORD 'demouser'; CREATE USER i2b2imdata WITH PASSWORD 'demouser'; CREATE USER i2b2metadata WITH PASSWORD 'demouser'; CREATE USER i2b2pm WITH PASSWORD 'demouser'; CREATE USER i2b2workdata WITH PASSWORD 'demouser'; CREATE SCHEMA AUTHORIZATION i2b2crcdata; CREATE SCHEMA AUTHORIZATION i2b2hive; CREATE SCHEMA AUTHORIZATION i2b2imdata; CREATE SCHEMA AUTHORIZATION i2b2metadata; CREATE SCHEMA AUTHORIZATION i2b2pm; CREATE SCHEMA AUTHORIZATION i2b2workdata;"
+    echo "importing the backup  of aktin and i2b2 databases"
+    sudo -u postgres psql -d i2b2 -f $backup_folder/backup_i2b2.sql
+    sudo -u postgres psql -d aktin -f $backup_folder/backup_aktin.sql
+}
+
+run_info_service() {
+    nc -w 2 127.0.0.1 1002
+}
+
+run_update_service() {
+    nc -w 2 127.0.0.1 1003
+}
+
+restart_aktin_services() {
+    echo "starting aktin services"
+    service apache2 start
+    service postgresql restart
+    service wildfly start
+}
+
+main() {
+    local backup_folder=$(extract_tarfile_and_get_folder_path $tarfile)
+    install_aktin_packages
+    stop_aktin_services
+    patch_aktin_properties "$backup_folder"
+    patch_wilfly_conf "$backup_folder"
+    import_folder_backup "$backup_folder" "/var/lib/aktin"
+    import_databases_backup "$backup_folder"
+    run_info_service
+    run_update_service
+    restart_aktin_services
+    remove_dir "$backup_folder"
+    echo "migration completed"
+}
+
+main | tee -a $log
---
+++ dwh-migration/create_aktin_backup.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#--------------------------------------
+# Script Name:  create_aktin_backup.sh
+# Author:       akombeiz@ukaachen.de
+# Date:         25 Apr 24
+# Purpose:      Backups database and configuration files
+#               of an AKTIN DWH as a tar.gz. Used for the
+#               migration from version 1.4 (install-script)
+#               to version 1.5 (debian package)
+#--------------------------------------
+
+set -euo pipefail
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+# create timestamp and log file
+readonly current=$(date +%Y_%h_%d_%H%M)
+readonly log=create_aktin_backup_$current.log
+
+# get wildfly home directory
+if [[ -d /opt/wildfly/ ]]; then
+    readonly wildfly_home=/opt/wildfly
+elif [[ -n $(ls /opt/wildfly-*) ]]; then
+    readonly wildfly_home=/opt/wildfly-*
+fi
+
+create_dir() {
+    local dir=$1
+    if [ ! -d $dir ]; then
+        mkdir $dir
+    fi
+    echo $dir
+}
+
+remove_dir() {
+    rm -rf $1
+}
+
+tar_dir()  {
+    tar -czf aktin_backup_$current.tar.gz --absolute-names --warning=no-file-changed $1/*
+}
+
+backup_file() {
+    local file=$1
+    local destination=$2
+    echo -e "backing up $file"
+    cp $file $destination
+}
+
+backup_folder() {
+    local folder=$1
+    local destination=$2
+    echo -e "backing up $folder"
+    cp -r $folder/* $destination
+}
+
+backup_database() {
+    local db=$1
+    local destination=$2
+    echo -e "backing up $db"
+    sudo -u postgres pg_dump $db >$destination
+}
+
+main() {
+    local tmp_dir=$(create_dir "backup_$current")
+
+    backup_file "/etc/aktin/aktin.properties" "$tmp_dir/backup_aktin.properties"
+    backup_file "$wildfly_home/standalone/configuration/standalone.xml" "$tmp_dir/backup_standalone.xml"
+    backup_file "$wildfly_home/bin/standalone.conf" "$tmp_dir/backup_standalone.conf"
+    create_dir "$tmp_dir/var"
+    create_dir "$tmp_dir/var/lib"
+    create_dir "$tmp_dir/var/lib/aktin"
+    backup_folder "/var/lib/aktin" "$tmp_dir/var/lib/aktin"
+    backup_database "i2b2" "$tmp_dir/backup_i2b2.sql"
+    backup_database "aktin" "$tmp_dir/backup_aktin.sql"
+
+    tar_dir "$tmp_dir"
+    remove_dir "$tmp_dir"
+    echo "backup completed"
+}
+
+main | tee -a $log
