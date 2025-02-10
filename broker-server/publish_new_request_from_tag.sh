32b3dcdf4b85078672eb1026a0df6c2d0d8a59f2
akomii <alexkombie@gmail.com>
2025-02-10 15:51:43 +0100

    some cleanup - updated metadata - updated comments - added [days_to_increase] as an optional flag to control time period - refactored `increase_ref_date_in_...` to a single generified method - added checks if tag list is empty or request does not have target nodes

--- broker-server/publish_new_request_from_tag.sh
+++ broker-server/publish_new_request_from_tag.sh
@@ -2,83 +2,70 @@
 #--------------------------------------
 # Script Name:  publish_new_request_from_tag.sh
 # Version:      1.0
-# Author:       whoy@ukaachen.de
-# Date:         05 Feb 25
-# Purpose:      Uploads a new request of given tag, using the last requests' data and new timestamps
+# Author:       whoy@ukaachen.de, akombeiz@ukaachen.de
+# Purpose:      Automates the duplication of a tagged request by retrieving the latest matching
+#               request, updating its dates, and publishing it to the broker.
 #--------------------------------------
 
 set -euo pipefail
 
-# ensure the correct number of arguments is provided
 [[ $# -lt 3 ]] && {
-  echo "Usage: $0 <broker_url> <admin_api_key> <tag>"
+  echo "Usage: $0 <broker_url> <admin_api_key> <tag> [days_to_increase]" >&2
+  echo "  <broker-url>       Required: The base URL of the broker" >&2
+  echo "  <admin-api-key>    Required: The admin API key for authentication" >&2
+  echo "  <tag>              Required: The tag to filter requests" >&2
+  echo "  [days-to-increase] Optional: Number of days to increase dates (default: 7)" >&2
   exit 1
 }
 
 readonly broker_url="$1/broker"
 readonly admin_api_key="$2"
 readonly tag="$3"
+readonly days_to_increase="${4:-7}"
 
-# retireve list of requests containing given tag
+# retrieve list of requests containing given tag
 readonly tagged_requests=$(curl -s --request GET "$broker_url/request/filtered?type=application/vnd.aktin.query.request%2Bxml&predicate=//queryRequest/query/principal/tags/tag='$tag'" --header "Authorization: Bearer $admin_api_key")
+
+# Exit script if no matching requests found
+if ! echo "$tagged_requests" | grep -q "<request "; then
+  echo "No requests found with tag '$tag'. Exiting."
+  exit 0
+fi
 
 # get id of last request with given tag
 readonly max_tagged_id=$(echo "$tagged_requests" | grep -oP '(?<=request id=")[0-9]+' | sort -nr | head -n1)
 
-# created new request on broker
-allocate_res=$(curl -s -i --request POST "$broker_url/request" --header "Authorization: Bearer $admin_api_key")
-new_request_id=$(echo "$allocate_res" | grep -oE 'Location: .*/[0-9]+' | grep -oE '[0-9]+$')
+# allocate new request on broker
+readonly allocate_res=$(curl -s -i --request POST "$broker_url/request" --header "Authorization: Bearer $admin_api_key")
+readonly new_request_id=$(echo "$allocate_res" | grep -oE 'Location: .*/[0-9]+' | grep -oE '[0-9]+$')
 
-# get last request content
+increase_date_in_request_content() {
+  local content="$1"
+  local element="$2"
+  local old_date=$(echo "$content" | sed -n "s/.*<$element>\(.*\)<\/$element>.*/\1/p")
+  local new_date=$(date -u -d "$old_date + $days_to_increase days" +"%Y-%m-%dT%H:%M:%S.000Z")
+  echo "$content" | sed "s|<$element>$old_date</$element>|<$element>$new_date</$element>|"
+}
+
+# get content of request with max id and increment id and update dates
 response=$(curl -s --request GET "$broker_url/request/$max_tagged_id" --header "Authorization: Bearer $admin_api_key")
+updated_response=$(echo "$response" | sed "s|<id>[0-9]*</id>|<id>$new_request_id</id>|")
+updated_response=$(increase_date_in_request_content "$updated_response" "reference")
+updated_response=$(increase_date_in_request_content "$updated_response" "scheduled")
 
-increase_ref_date_in_request_content() {
-  request_content="$1"
-
-  # Extract the 'reference' date from the content
-  reference_date=$(echo "$request_content" | sed -n 's/.*<reference>\(.*\)<\/reference>.*/\1/p')
-
-  # Add 7 days to the extracted reference date using the date command
-  new_reference_date=$(date -u -d "$reference_date + 7 days" +"%Y-%m-%dT%H:%M:%S.000Z")
-
-  # Replace the old reference date with the new one in the content using sed
-  new_response=$(echo "$request_content" | sed "s|<reference>$reference_date</reference>|<reference>$new_reference_date</reference>|")
-
-  echo "$new_response"
-}
-
-increase_scheduled_date_in_request_content() {
-  request_content="$1"
-
-  # Extract the 'reference' date from the content
-  old_date=$(echo "$request_content" | sed -n 's/.*<scheduled>\(.*\)<\/scheduled>.*/\1/p')
-
-  # Add 7 days to the extracted reference date using the date command
-  new_date=$(date -u -d "$old_date + 7 days" +"%Y-%m-%dT%H:%M:%S.000Z")
-
-  # Replace the old reference date with the new one in the content using sed
-  new_response=$(echo "$request_content" | sed "s|<scheduled>$old_date</scheduled>|<scheduled>$new_date</scheduled>|")
-
-  echo "$new_response"
-}
-
-# id, reference und scheduled ändern
-response_id_inc=$(echo "$response" | sed "s|<id>[0-9]*</id>|<id>$new_request_id</id>|")
-response_ref_inc=$(increase_ref_date_in_request_content "$response_id_inc")
-response_sched_inc=$(increase_scheduled_date_in_request_content "$response_ref_inc")
-
-# add content to new allocated request
-curl -s --request PUT "$broker_url/request/$new_request_id" --data-raw "$response_sched_inc" --header "Authorization: Bearer $admin_api_key" --header "Content-Type: application/vnd.aktin.query.request+xml"
+# put updated content to allocated request
+curl -s --request PUT "$broker_url/request/$new_request_id" --data-raw "$updated_response" --header "Authorization: Bearer $admin_api_key" --header "Content-Type: application/vnd.aktin.query.request+xml"
 
 # get targeted nodes
-readonly nodes=$(curl -s --request GET "$broker_url/request/$max_tagged_id/nodes" --header "Authorization: Bearer $admin_api_key")
+target_nodes_response=$(curl -s -w "%{http_code}" --request GET "$broker_url/request/$max_tagged_id/nodes" --header "Authorization: Bearer $admin_api_key")
+target_nodes="${target_nodes_response::-3}"
+http_code="${target_nodes_response: -3}"
 
-# add target nodes in new request
-curl -s --request PUT "$broker_url/request/$new_request_id/nodes" --data-raw "$nodes" --header "Authorization: Bearer $admin_api_key" --header "Content-Type: application/xml"
+# if GET request is successful, add target nodes in new request
+# if no target nodes are set, request is global
+if [[ "$http_code" -ne 404 ]]; then
+  curl -s --request PUT "$broker_url/request/$new_request_id/nodes" --data-raw "$target_nodes" --header "Authorization: Bearer $admin_api_key" --header "Content-Type: application/xml"
+fi
 
-#publish new request
+# publish new request
 curl -s --request POST "$broker_url/request/$new_request_id/publish" --header "Authorization: Bearer $admin_api_key"
-
-
-
-
